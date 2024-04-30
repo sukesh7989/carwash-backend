a1ae00b41d0edfdde112bf41d717a705f1a25799
sukesh7989 <sukesh.ketha@gmail.com>
2024-04-30 12:25:17 +0530

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const corsOptions = {
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', "http://localhost:5173/");
-...
+
 });
 //middleware
 server.use(cors(corsOptions))
