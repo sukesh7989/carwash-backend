81784cedaff015662627beddcb4352210ab1b56a
sukesh7989 <sukesh.ketha@gmail.com>
2024-04-30 12:24:46 +0530

    cors error

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ const corsOptions = {
   optionSuccessStatus: 200
 }
 
-
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', "http://localhost:5173/");
+...
+});
 //middleware
 server.use(cors(corsOptions))
 server.use(express.json())
